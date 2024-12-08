829d84e23f4c5b0af84ffc4598b31de601af195e
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-12-08 18:16:46 -0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,45 @@
+import ffmpeg from "fluent-ffmpeg"
+import { basename, extname, join } from "path"
+import { existsSync, mkdirSync } from "fs"
+
+function extractAudio(videoPath, outputDir = ".") {
+  return new Promise((resolve, reject) => {
+    if (!existsSync(videoPath)) {
+      return reject(new Error(`Arquivo de vídeo não encontrado: ${videoPath}`))
+    }
+
+    const outputFileName = `${basename(videoPath, extname(videoPath))}.mp3`
+    console.log({ outputFileName })
+    const outputPath = join(outputDir, outputFileName)
+
+    if (!existsSync(outputDir)) {
+      mkdirSync(outputDir, { recursive: true })
+    }
+
+    ffmpeg(videoPath)
+      .output(outputPath)
+      .audioCodec("libmp3lame")
+      .on("end", () => {
+        console.log(`Áudio extraído com sucesso: ${outputPath}`)
+        resolve(outputPath)
+      })
+      .on("error", (err) => {
+        console.error(`Erro ao extrair áudio: ${err.message}`)
+        reject(err)
+      })
+      .run()
+  })
+}
+
+;(async () => {
+  const videoPath = "/home/robert/2024-12-08 17-46-29.mkv"
+  const outputDir =
+    "/media/robert/storage/Central/projetos/games/C++/sdl/cavestory/implementations/my-cavestory/content/sounds/background.mp3"
+
+  try {
+    const audioPath = await extractAudio(videoPath, outputDir)
+    console.log(`Áudio salvo em: ${audioPath}`)
+  } catch (error) {
+    console.error(`Falha ao extrair áudio: ${error.message}`)
+  }
+})()
